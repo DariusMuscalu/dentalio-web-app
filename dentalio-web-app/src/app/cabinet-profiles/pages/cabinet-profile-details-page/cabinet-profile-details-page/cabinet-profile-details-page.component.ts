f6bb1d146f1f8527eb5402d12a98ec38af97a3d5
darius <muscaludarius4@gmail.com>
2023-11-29 19:23:06 +0200

    Displayed more data and modified the styling in cabinet profile details page

--- dentalio-web-app/src/app/cabinet-profiles/pages/cabinet-profile-details-page/cabinet-profile-details-page/cabinet-profile-details-page.component.ts
+++ dentalio-web-app/src/app/cabinet-profiles/pages/cabinet-profile-details-page/cabinet-profile-details-page/cabinet-profile-details-page.component.ts
@@ -1,46 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { AppState } from 'src/app/state/app.state';
 import {
   selectCabinetProfileById,
   selectCabinetProfileByIdStatus,
 } from 'src/app/state/cabinet-profiles/cabinet-profiles.selectors';
 import { fetchCabinetProfileById } from 'src/app/state/cabinet-profiles/cabinet-profiles.actions';
+import { CabinetProfileM } from 'src/app/cabinet-profiles/models/cabinet-profile.model';
 
 @Component({
   selector: 'app-cabinet-profile-details-page',
   templateUrl: './cabinet-profile-details-page.component.html',
   styleUrls: ['./cabinet-profile-details-page.component.scss'],
 })
-export class CabinetProfileDetailsPageComponent implements OnInit {
-  public selectedCabinetProfile$ = this.store.select(selectCabinetProfileById);
-  public status$ = this.store.select(selectCabinetProfileByIdStatus);
+export class CabinetProfileDetailsPageComponent implements OnInit, OnDestroy {
+  public selectedCabinetProfile$: Observable<CabinetProfileM> =
+    this.store.select(selectCabinetProfileById);
+  public status$: Observable<string> = this.store.select(
+    selectCabinetProfileByIdStatus
+  );
+  private destroy$: Subject<void> = new Subject<void>();
 
   constructor(private store: Store<AppState>, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params) => {
+    this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
       const cabinetId = params['cabinetId'];
 
       // Dispatch the action with the cabinetId
       this.store.dispatch(fetchCabinetProfileById({ cabinetId }));
     });
 
-    this.status$.subscribe((status) => {
-      // DEBUGGING PURPOSES
-      // You can perform actions based on the status here
-      if (status === 'loading') {
-        // Do something when loading
-      } else if (status === 'success') {
-        // Access the selected cabinet data when the status is 'success'
-        this.selectedCabinetProfile$.subscribe((cabinet) => {
-          console.log('Selected Cabinet Data:', cabinet);
-        });
-        // Do something when successful
-      } else if (status === 'error') {
-        // Do something when there's an error
-      }
-    });
+    this.selectedCabinetProfile$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((cabinet) => {
+        // Access the selected cabinet data
+        console.log('Selected Cabinet Data:', cabinet);
+      });
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe from Observables to prevent memory leaks
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
