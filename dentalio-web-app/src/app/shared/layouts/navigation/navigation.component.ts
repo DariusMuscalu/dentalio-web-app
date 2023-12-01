e2f6af5d0f5efbca462cccba5dbda43aef04ab48
darius <muscaludarius4@gmail.com>
2023-12-01 10:27:38 +0200

    Added signOut from auth service to work in the nav when tapping exit account btn

--- dentalio-web-app/src/app/shared/layouts/navigation/navigation.component.ts
+++ dentalio-web-app/src/app/shared/layouts/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationService } from './navigation.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -9,9 +10,16 @@ import { NavigationService } from './navigation.service';
 export class NavigationComponent {
   isNavMenuDisplayed: boolean = false;
 
-  constructor(private navigationService: NavigationService) {}
+  constructor(
+    private navigationService: NavigationService,
+    private authService: AuthService
+  ) {}
 
   toggleNavMenuVisibility() {
     this.navigationService.toggleNavMenuVisibility();
+  }
+
+  signOut() {
+    this.authService.SignOut();
   }
 }
