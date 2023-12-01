00df3a94cd460cda3c2b94991c274f077863d9ba
darius <muscaludarius4@gmail.com>
2023-12-01 10:32:58 +0200

    Removed edit profile page, because editing will be posible directly into the profile page

--- dentalio-web-app/src/app/profile/profile-routing.module.ts
+++ dentalio-web-app/src/app/profile/profile-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
-import { EditProfilePageComponent } from './pages/edit-profile-page/edit-profile-page.component';
 
-const routes: Routes = [
-  { path: '', component: ProfilePageComponent },
-  { path: 'edit', component: EditProfilePageComponent },
-];
+const routes: Routes = [{ path: '', component: ProfilePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- dentalio-web-app/src/app/profile/profile.module.ts
+++ dentalio-web-app/src/app/profile/profile.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
-import { EditProfilePageComponent } from './pages/edit-profile-page/edit-profile-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [ProfilePageComponent, EditProfilePageComponent],
+  declarations: [ProfilePageComponent],
   imports: [CommonModule, SharedModule, ProfileRoutingModule, FormsModule],
 })
 export class ProfileModule {}
