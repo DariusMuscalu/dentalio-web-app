4f565744caae19a232d105d06aa18c672b7e9868
darius <muscaludarius4@gmail.com>
2023-12-01 18:19:48 +0200

    Added appointment card component and finished it's design

--- dentalio-web-app/src/app/appointments/appointments.module.ts
+++ dentalio-web-app/src/app/appointments/appointments.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { AppointmentsRoutingModule } from './appointments-routing.module';
 import { AppointmentsComponent } from './appointments.component';
-
+import { AppointmentCardComponent } from './layouts/appointment-card/appointment-card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppointmentsComponent
-  ],
+  declarations: [AppointmentsComponent, AppointmentCardComponent],
   imports: [
     CommonModule,
-    AppointmentsRoutingModule
-  ]
+    AppointmentsRoutingModule,
+    SharedModule,
+    RouterModule,
+  ],
 })
-export class AppointmentsModule { }
+export class AppointmentsModule {}
