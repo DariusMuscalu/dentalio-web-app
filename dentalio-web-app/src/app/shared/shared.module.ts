a3dcb26e38622dbd0e8c5c6d89f373905b1d54fc
darius <muscaludarius4@gmail.com>
2023-12-01 10:11:11 +0200

    Moved footer component into the shared/layouts folder

--- dentalio-web-app/src/app/shared/shared.module.ts
+++ dentalio-web-app/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoriteButtonComponent } from './layouts/favorite-button/favorite-button.component';
 import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from '../footer/footer/footer.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 import { NavigationComponent } from './layouts/navigation/navigation.component';
 import { AuthModule } from '../auth/auth.module';
 import { RouterModule } from '@angular/router';
